11f987a9fa520fb94ed682ed8b924722e2635aae
wangpeiyang <wpyok500@gmail.com>
2022-04-07 10:01:26 +0800

    添加项目文件。

---
+++ 直播源整理/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace 直播源整理
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请将txt文本拖进来";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 98);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(255, 137);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(255, 137);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "直播源整理";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 直播源整理/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 直播源整理
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            Array array = (Array)(e.Data.GetData(DataFormats.FileDrop));
+            string filepath = array.GetValue(0).ToString();
+            string path = filepath.Substring(0, filepath.LastIndexOf("\\"));
+            string filenaem = filepath.Substring(filepath.LastIndexOf("\\")+1);
+
+            List<string> list = File.ReadAllLines(filepath).ToList();
+            Dictionary<string, string> tvdic = new Dictionary<string, string>();
+            foreach (string item in list.ToArray())
+            {
+                string[] temp = item.Split(',');
+                if (temp.Length == 1 || item.Contains("#genre#"))
+                {
+                    continue;
+                }
+                if (tvdic.Count == 0)
+                {
+                    tvdic.Add(temp[0], temp[1]);
+                }
+                else
+                {
+                    foreach (KeyValuePair<string, string> item1 in tvdic)
+                    {
+                        KeyValuePair<string, string> tt = tvdic.FirstOrDefault(x => x.Key.Equals(temp[0]));
+                        if (tt.Key == null)
+                        {
+                            if (item1.Key.Equals(temp[0]))
+                            {
+                                if (tvdic[temp[0]].Contains(temp[1]))
+                                {
+                                    continue;
+                                }
+                                tvdic[temp[0]] = tvdic[temp[0]] + "#" + temp[1];
+                                break;
+                            }
+                            else
+                            {
+
+                                tvdic.Add(temp[0], temp[1]);
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if (tvdic[temp[0]].Contains(temp[1]))
+                            {
+                                continue;
+                            }
+                            tvdic[temp[0]] = tvdic[temp[0]] + "#" + temp[1];
+                            break;
+                        }
+
+                    }
+                }
+
+
+
+            }
+            List<string> list1 = new List<string>();
+            foreach (KeyValuePair<string, string> item in tvdic)
+            {
+                list1.Add(item.Key + "," + item.Value);
+            }
+            list1.Sort((x, y) => -x.CompareTo(y));
+            string time = DateTime.Now.ToString("yyMMddHHmmss");
+            //File.WriteAllLines(System.Environment.CurrentDirectory + $"\\sort{time}.txt", list1.ToArray());
+            filenaem = filenaem.Replace(".", "_sort.");
+            File.WriteAllLines(path + $"\\{filenaem}.txt", list1.ToArray());
+            MessageBox.Show("整理完成");
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+    }
+}
